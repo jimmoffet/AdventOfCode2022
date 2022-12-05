f3d2ae5c7703eea87a36e39a32aee45608f5c828
jimmoffet <jim.moffet@gsa.gov>
2022-12-05 10:00:59 -0800

    I do not think that word means what you think it means

--- day4/day4.py
+++ day4/day4.py
@@ -41,15 +41,15 @@ def oneline_sans_file_open(input):
     return sum([1 if ((assignments[0] >= assignments[2] and assignments[1] <= assignments[3]) or (assignments[0] <= assignments[2] and assignments[1] >= assignments[3]) ) else 0 for assignments in [list(map(int, line.replace("-",",").split(","))) for line in input ]])
 
 n = open(f,'r').read().splitlines()
-scalar = 10
-n_scaled = n * scalar
+scaling_factor = 10
+n_scaled = n * scaling_factor
 f1 = 'oneline_sans_file_open(n)'
 f2 = 'oneline_sans_file_open(n_scaled)'
 func = 'oneline_sans_file_open'
 def get_variance(f1, f2, iter):
     input_time_n = timeit.timeit(f1, number=iter, globals=globals())
     input_time_n_scaled = timeit.timeit(f2, number=iter, globals=globals())
-    input_time_n_expected = input_time_n_scaled / scalar
+    input_time_n_expected = input_time_n_scaled / scaling_factor
     return abs((input_time_n / input_time_n_expected) - 1) 
 
 variance = get_variance(f1, f2, 100)
