75afc1b8a6c827b93b88f414f788ba04ed284832
jimmoffet <jim.moffet@gsa.gov>
2022-12-04 16:18:36 -0800

    clean up

--- day4/day4.py
+++ day4/day4.py
@@ -3,39 +3,17 @@ import sys
 
 f = 'input_day4.txt'
 
-sample = [
-    [94, 95, 61, 94],
-    [38, 99, 39, 73], # yes 
-    [6, 11, 11, 84], 
-    [29, 49, 9, 29], 
-    [24, 91, 24, 44], # yes
-    [72, 72, 28, 72], # yes
-    [28, 72, 72, 72], # yes
-    [72, 72, 72, 72], # yes
-    [49, 53, 49, 51], # yes
-    [9, 85, 8, 85], # yes
-    [26, 80, 26, 90] # yes
-    ] # count should be 8 yes, 3 no
-
-inp = open(f,'r').read().splitlines()
-pairs = [list(map(int, line.replace("-",",").split(","))) for line in inp]
-multiline_count = sum(
-    [
-        1 if (
-            (lst[0] >= lst[2] and lst[1] <= lst[3]) or 
-            (lst[0] <= lst[2] and lst[1] >= lst[3]) 
-        ) else 0 for lst in pairs
-    ]
-)
-print(f'multiline_count: {multiline_count}')
-
 oneline_count = sum([1 if ((lst[0] >= lst[2] and lst[1] <= lst[3]) or (lst[0] <= lst[2] and lst[1] >= lst[3]) ) else 0 for lst in [list(map(int, line.replace("-",",").split(","))) for line in open(f,'r').read().splitlines()]])
+
 print(f'oneline_count: {oneline_count}')
+
+###################################################################
+# Check that we're within 50% of the expected time for O(n) runtime
+###################################################################
 
 def oneline_sans_file_open(inp4):
     return sum([1 if ((lst[0] >= lst[2] and lst[1] <= lst[3]) or (lst[0] <= lst[2] and lst[1] >= lst[3]) ) else 0 for lst in [list(map(int, line.replace("-",",").split(","))) for line in inp4 ]])
 
-# Check that we're within 10% of the expected time for O(n) runtime
 n = open(f,'r').read().splitlines()
 scalar = 10
 n_scaled = n * scalar
