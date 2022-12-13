b5a25c165ce9eed5c7808c18a481c4043674f0a3
jimmoffet <jim.moffet@gsa.gov>
2022-12-12 23:57:11 -0800

    cleaned up pretty well

--- day12/day12.py
+++ day12/day12.py
@@ -16,36 +16,31 @@ steps = [
                 start_coords, 
                 end_coords, 
                 next(
-                    G.remove_edges_from(list(
-                        e[:2] for e in [
-                            (a,b) for a, b in G.edges() 
-                            if 
-                            (array2D[a[0]][a[1]]) - (array2D[b[0]][b[1]]) < -1
-                        ]
-                    )) 
-                    or G for G in 
+                    G.remove_edges_from([
+                        (a,b)[:2] for a, b in G.edges() 
+                        if 
+                        (array2D[a[0]][a[1]]) - (array2D[b[0]][b[1]]) < -1
+                    ]) or G for G in 
                     (nx.grid_2d_graph(*array2D.shape).to_directed(),) # one item tuple
                 )
             ) 
             for start_coords, end_coords, array2D in 
-            [
-                ( 
+            ((
+                lambda start_coord_pairs, end_coord_pairs, array2D : ( 
                     tuple(map(sum, zip(*start_coord_pairs))), 
                     tuple(map(sum, zip(*end_coord_pairs))), 
                     np.array(array2D) 
-                ) 
-                for start_coord_pairs, end_coord_pairs, array2D in 
-                (tuple(
-                    map(list, zip(*[
-                        ( 
-                            ((i, line.index('S')) if 'S' in line else (0,0)),
-                            ((i, line.index('E')) if 'E' in line else (0,0)),
-                            [1 if char=='S' else 26 if char=='E' else ord(char) - 96 for char in line]
-                        )
-                        for i, line in enumerate(open(f,'r').read().splitlines())
-                    ]))
-                ),) # one item tuple
-            ]
+                )
+            )(
+                *map(list, zip(*[
+                    ( 
+                        ((i, line.index('S')) if 'S' in line else (0,0)),
+                        ((i, line.index('E')) if 'E' in line else (0,0)),
+                        [1 if char=='S' else 26 if char=='E' else ord(char) - 96 for char in line]
+                    )
+                    for i, line in enumerate(open(f,'r').read().splitlines())
+                ]))
+            ),) # one item tuple
         ]
     ][0][0],) # one item tuple
 ][0]
