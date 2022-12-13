1e43e773079cfd3a9cb46a3063117c42a086d0d0
jimmoffet <jim.moffet@gsa.gov>
2022-12-12 22:56:33 -0800

    cleaned up pretty well

--- day12/day12 archive.py
+++ day12/day12 archive.py
@@ -5,15 +5,91 @@ import numpy as np
 import sys
 
 
-# with open("graph.txt") as f:
-#     lines = f.readlines()
+# steps = [nx.shortest_path_length(G, source=start_coords, target=end_coords) for start_coords, end_coords, G in ([[(start_coords, end_coords, [G.remove_edges_from(list(e[:2] for e in [(a,b,attrs) for a, b, attrs in G.edges(data=True) 
+#     if (
+#         ( 
+#             (ar[a[0]][a[1]]) - (ar[b[0]][b[1]]) < -1 
+#             and 
+#             ar[a[0]][a[1]] != 0 
+#             and 
+#             ar[b[0]][b[1]] != 27
+#         ) or (
+#             (ar[a[0]][a[1]]) - (ar[b[0]][b[1]]) < -2
+#             and 
+#             (ar[a[0]][a[1]] == 0 
+#             or 
+#             ar[b[0]][b[1]] == 27)
+#         )
+#     )
+# ])) or G for G in (nx.grid_2d_graph(*ar.shape).to_directed(),)][0]) for start_coords, end_coords, ar in (( next(zip(*np.where(ar == 0))), next(zip(*np.where(ar == 27))), ar),) ] for ar in (np.array([[ord(char) - 96 if char.islower() else 0 if char=='S' else 27 for i, char in enumerate(line)] for i, line in enumerate(open(f,'r').read().splitlines())]),)][0][0],)][0]
 
-# edgeList = [line.strip().split() for line in lines]
-
-edgeList = [(0,1), (1,6), (6,2), (2,8), (8,7), (7,9), (9,10), (0,4), (4,5), (5,6), (8,10)]
+# print(f'shortest path length: {steps}')
 
 # f = 'day12_test.txt'
 f = 'day12.txt'
+f = 'day12_test.txt'
+# f = 'day12.txt'
+
+# steps = [
+#     nx.shortest_path_length(G, source=start_coords, target=end_coords) 
+#     for start_coords, end_coords, G in 
+#     (
+#         [
+#             [
+#                 (
+#                     start_coords, 
+#                     end_coords, 
+#                     [
+#                         G.remove_edges_from(
+#                             list(
+#                                 e[:2] for e in [
+#                                     (a,b,attrs) for a, b, attrs in G.edges(data=True) 
+#                                     if (
+#                                         ( 
+#                                             (ar[a[0]][a[1]]) - (ar[b[0]][b[1]]) < -1 
+#                                             and 
+#                                             ar[a[0]][a[1]] != 0 
+#                                             and 
+#                                             ar[b[0]][b[1]] != 27
+#                                         ) 
+#                                         or 
+#                                         (
+#                                             (ar[a[0]][a[1]]) - (ar[b[0]][b[1]]) < -2
+#                                             and 
+#                                             (ar[a[0]][a[1]] == 0 
+#                                             or 
+#                                             ar[b[0]][b[1]] == 27)
+#                                         )
+#                                     )
+#                                 ]
+#                             )
+#                         ) 
+#                         or G for G in 
+#                         (nx.grid_2d_graph(*ar.shape).to_directed(),) # one item tuple
+#                     ][0]
+#                 ) 
+#                 for start_coords, end_coords, ar 
+#                 in 
+#                 (( 
+#                     next(zip(*np.where(ar == 0))), 
+#                     next(zip(*np.where(ar == 27))), 
+#                     ar
+#                 ),) # one item tuple
+#             ] 
+#             for ar in 
+#             (
+#                 np.array(
+#                     [
+#                         [ord(char) - 96 if char.islower() else 0 if char=='S' else 27 for i, char in enumerate(line)] 
+#                         for i, line in enumerate(open(f,'r').read().splitlines())
+#                     ]
+#                 ),
+#             )
+#         ][0][0],
+#     )
+# ][0]
+
+# print(f'shortest path length: {steps}')
 
 
 # enumerated_lines = enumerate(open(f,'r').read().splitlines())
--- day12/day12.py
+++ day12/day12.py
@@ -7,22 +7,49 @@ import sys
 # f = 'day12_test.txt'
 f = 'day12.txt'
 
-steps = [nx.shortest_path_length(G, source=start_coords, target=end_coords) for start_coords, end_coords, G in ([[(start_coords, end_coords, [G.remove_edges_from(list(e[:2] for e in [(a,b,attrs) for a, b, attrs in G.edges(data=True) 
-    if (
-        ( 
-            (ar[a[0]][a[1]]) - (ar[b[0]][b[1]]) < -1 
-            and 
-            ar[a[0]][a[1]] != 0 
-            and 
-            ar[b[0]][b[1]] != 27
-        ) or (
-            (ar[a[0]][a[1]]) - (ar[b[0]][b[1]]) < -2
-            and 
-            (ar[a[0]][a[1]] == 0 
-            or 
-            ar[b[0]][b[1]] == 27)
-        )
-    )
-])) or G for G in (nx.grid_2d_graph(*ar.shape).to_directed(),)][0]) for start_coords, end_coords, ar in (( next(zip(*np.where(ar == 0))), next(zip(*np.where(ar == 27))), ar),) ] for ar in (np.array([[ord(char) - 96 if char.islower() else 0 if char=='S' else 27 for i, char in enumerate(line)] for i, line in enumerate(open(f,'r').read().splitlines())]),)][0][0],)][0]
+steps = [
+    nx.shortest_path_length(G, source=start_coords, target=end_coords) 
+    for start_coords, end_coords, G in 
+    ([
+        [
+            (
+                start_coords, 
+                end_coords, 
+                [
+                    G.remove_edges_from(list(
+                        e[:2] for e in [
+                            (a,b) for a, b in G.edges() 
+                            if 
+                            (array2D[a[0]][a[1]]) - (array2D[b[0]][b[1]]) < -1
+                        ]
+                    )) 
+                    or G for G in 
+                    (nx.grid_2d_graph(*array2D.shape).to_directed(),) # one item tuple
+                ][0]
+            ) 
+            for start_coords, end_coords, array2D in 
+            ([
+                [
+                    ( 
+                        tuple(map(sum, zip(*Scoord_Ecoord_array[0]))), 
+                        tuple(map(sum, zip(*Scoord_Ecoord_array[1]))), 
+                        np.array(Scoord_Ecoord_array[2]) 
+                    ) 
+                    for Scoord_Ecoord_array in 
+                    (list(
+                        map(list, zip(*[
+                            ( 
+                                ((i, line.index('S')) if 'S' in line else (0,0)),
+                                ((i, line.index('E')) if 'E' in line else (0,0)),
+                                [1 if char=='S' else 26 if char=='E' else ord(char) - 96 for char in line]
+                            )
+                            for i, line in enumerate(open(f,'r').read().splitlines())
+                        ]))
+                    ),) # one item tuple
+                ][0]
+            ][0],) # one item tuple
+        ]
+    ][0][0],) # one item tuple
+][0]
 
 print(f'shortest path length: {steps}')
